5719f028d1e28f80409845958088c70d5fbc81dd
Siddhant Anand <siddhantanand23@gmail.com>
2018-09-30 23:35:03 +1200

    Sunday

--- src/App.js
+++ src/App.js
@@ -2,57 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Modal from 'react-modal';
+import Layout from './View/Layout';
+import factory from './ethereum/factory'
 
 const API = 'http://localhost:4001/users';
 
 
 class App extends Component {
-  
-    constructor(props) {
-      super(props);
-  
-      this.state = { users: [] };
-    }
+  static async getInitialProps(){
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-    componentDidMount() {
-      fetch(API)
-        .then(response => response.json())
-        .then(data => this.setState({users:data}));
-    }
-
-    render() {
-      const { users } = this.state;
+    return {campaigns};
+  }
   
+  render() {  
       return (
         <div>
-          Hello
-            {users.map(user =>
-              <li key={user.user_id}>
-                {user.user_firstname} | {user.user_lastname}
-              </li>
-            )}
+          <div>{this.props.campaigns[0]}</div>
+          <Layout>
+            <div className="App">
+              <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+              </header>
+              <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+              </p>
+            </div>
+          </Layout>
         </div>
-
       );
     }
-
-  
-  
-  
-  
-  // render() {
-  //   return (
-  //     <div className="App">
-  //       <header className="App-header">
-  //         <img src={logo} className="App-logo" alt="logo" />
-  //         <h1 className="App-title">Welcome to React</h1>
-  //       </header>
-  //       <p className="App-intro">
-  //         To get started, edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //     </div>
-  //   );
-  // }
 }
 
 export default App;
